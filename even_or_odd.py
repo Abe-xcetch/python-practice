907745bc31f9bf47c7c85df34a335e6f34db7fe5
Abe-xcetch <Kilwakeabe4@gmail.com>
2025-04-26 21:43:20 +0300

    Initial commit

---
+++ even_or_odd.py
@@ -0,0 +1,7 @@
+# check if the number is even or odd
+number = 7
+
+if number % 2 == 0:
+    print(number, "is  even")
+else:
+    print(number, "is  odd")
---
+++ sum_list.py
@@ -0,0 +1,9 @@
+# Summing all the numbers in a list
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+total = 0
+
+for number in numbers:
+    total += number
+
+print(total)
